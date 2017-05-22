66ff0055aed589daca9c017b2689ac5c5e287a9f
lordaniket06 <aniketp@iitk.ac.in>
2017-05-23 02:42:59 +0530

    Hacky fix for admin-error

--- gymkhana/urls.py
+++ gymkhana/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import RedirectView
-from django.contrib.auth.urls import views
+# from django.contrib.auth.urls import views
 
 
 urlpatterns = [
